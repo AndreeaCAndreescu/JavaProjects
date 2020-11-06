516fea174b7cd4af2367e97802cd63be95ea0601
AndreeaCAndreescu <65339651+AndreeaCAndreescu@users.noreply.github.com>
2020-11-06 13:20:46 +0200

    Add files via upload

---
+++ CharAndBoolean/src/academy/learnprogramming/Main.java
@@ -0,0 +1,19 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       char myChar = 'D';
+       char myUnicodeChar = '\u0044'; // u de la unicode si 0044 este codul asociat literei D
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwentyOne = true;
+
+    }
+}
---
+++ Classes/src/academy/learnprogramming/Main.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porche=new Car();
+        Car holden=new Car();
+        porche.setModel("Carrera");
+        System.out.println("Model is "+porche.getModel());
+
+    }
+}
+
+
