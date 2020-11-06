f00860d5777839e4eafa7d1f801808770f52cfe8
AndreeaCAndreescu <65339651+AndreeaCAndreescu@users.noreply.github.com>
2020-11-06 13:27:17 +0200

    Add files via upload

---
+++ OOP Exercise/src/academy/learnprogramming/Main.java
@@ -0,0 +1,23 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Hamburger hamburger=new Hamburger("Basic","Sousage",3.36,"white");
+        double price=hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato",0.27);
+        hamburger.addHamburgerAddition2("Lettuce",0.75);
+        hamburger.addHamburgerAddition3("Cheese",1.12);
+        System.out.println("Total Burger price is "+hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger=new HealthyBurger("Bacon",5.67);
+        healthyBurger.addHamburgerAddition1("Egg",5.43);
+        healthyBurger.addHealthAddition1("Lentils",3.41);
+        System.out.println("Total Healthy price is " + healthyBurger.itemizeHamburger());
+
+        DeluxeBurger deluxeBurger=new DeluxeBurger();
+        deluxeBurger.addHamburgerAddition3("Should not do this",50.53);
+        deluxeBurger.itemizeHamburger();
+    }
+}
